60292b7057e8763e89d327f2cd7abb21eab1553d
Roman Zhornytskiy <roman.zhornytskiy@polymtl.ca>
2019-02-11 14:23:27 -0500

    la 3e et la 4e est faite... il reste juste la 5e

--- td2/factsage.js
+++ td2/factsage.js
@@ -1,4 +1,58 @@
 
+
+function affichage() {
+
+	document.getElementById("load").style.visibility = "visible";
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+
+		var  x, txt = "";
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+
+			console.log(myObj);
+			
+			var myTable= " <h3>" + myObj.formule + "</h3>" 
+			myTable += "<table>";
+			myTable += "<tr border-spacing='10px'> <th> Phase </th>  <th> mol </th> <th> Température (k) </th> <th> Pression (atm) </th> <th> Type </th> </tr>"; 
+			
+			for(var i=0; i<13; i++){
+				myTable += "<tr> <td >" + myObj.output1[i].libelle + "</td>" +
+				"<td align='center'>" + myObj.output1[i].concentration + "</td>" +
+				"<td align='center'>" + myObj.output1[i].conditions.temperature.valeur + "</td>" +
+				"<td align='center'>" + myObj.output1[i].conditions.pression.valeur + "</td>" +
+				"<td align='center'>" + myObj.output1[i].conditions.type + "</td>" ;
+			}
+			
+			myTable += "</table> <table> <tr class = 'trCustom'>";
+			//myTable += "<tr class = 'trCustom'> <th> Nom </th> <th> Unité </th> <th> Valeur </th>  </tr>";
+
+			for(var i=0;i<6;i++){
+				myTable += "<th >" + myObj.output2[i].libelle + "</th>";
+			}
+			myTable += "</tr> <tr class = 'trCustom'>";
+			for(var i=0;i<6;i++){
+				myTable += " <th >" + myObj.output2[i].unite + "</th>";
+			}
+			myTable+="</tr> <tr>"; 
+			for(var i=0;i<6;i++){
+				myTable +=" <td >" + myObj.output2[i].valeur + "</td>";
+			}
+			
+			myTable+="</tr> </table>";
+
+			document.getElementById('resultat').innerHTML = myTable;
+		}
+	};
+
+	xmlhttp.open("POST", "http://localhost:8080", true);
+	// était: "data-output.json"
+	xmlhttp.send(); //c'est ici qu'on peut envoyer notre donnée
+
+}
+
+/*
 function clearform() {
 	document.myform.REACT1.value = "";
 	document.myform.REACT2.value = "";
@@ -134,3 +188,4 @@ function MM_swapImage() { //v3.0
   var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
    if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
 }
+*/
